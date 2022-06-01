e0e5b6f8305d0be52a1e1d5c61931060d3b9d5ea
JacksonSmollen <104814686+JacksonSmollen@users.noreply.github.com>
2022-06-02 02:05:59 +1000

    boss and level fixes
    boss room added and level parts fixed
    sky box added and swords.

---
+++ Assets/Scripts/boss_scripts/lookat.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookat : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.LookAt(player.transform);
+
+        //transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 0f, 0f);
+    }
+}
