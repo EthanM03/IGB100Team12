0f73fe6cd1efa6711194a47add104b580eb8201c
DanieLowe <104814678+DanieLowe@users.noreply.github.com>
2022-06-07 21:46:25 +1000

    ui

--- Assets/Scripts/toggle_select.cs
+++ Assets/Scripts/toggle_select.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
 
 public class toggle_select : MonoBehaviour
 {
+    ToggleGroup toggleGroup;
+    
+    public Toggle currenSelection
+    {
+        get { return toggleGroup.ActiveToggles().FirstOrDefault(); }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        toggleGroup = GetComponent<ToggleGroup>();   
     }
 
     // Update is called once per frame
@@ -15,4 +23,10 @@ public class toggle_select : MonoBehaviour
     {
         
     }
+
+    public void select_Toggle(int id)
+    {
+
+    }
+    
 }
